284c70e32237b2029eac1069e3234c6ae916080b
javalenjara <Javalen git2016,>
2020-08-08 10:21:42 -0500

    Adding lombok to Estate entity
    Added lombok anotations to Estate entity

--- src/main/java/co/com/udem/rea/dto/EstateDTO.java
+++ src/main/java/co/com/udem/rea/dto/EstateDTO.java
@@ -2,10 +2,17 @@ package co.com.udem.rea.dto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EstateDTO {
 
 	private Long id;
-	private String estateCode;
+	private int estateCode;
 	private float area;
 	private int numOfRooms;
 	private int numOfBathrooms;
@@ -14,86 +21,5 @@ public class EstateDTO {
 	
 	@Autowired
 	private UserDTO userDTO;
-	
-	public EstateDTO(Long id, String estateCode, float area, int numOfRooms, int numOfBathrooms, boolean isForSale,
-			float estateValue, UserDTO userDTO) {
-		super();
-		this.id = id;
-		this.estateCode = estateCode;
-		this.area = area;
-		this.numOfRooms = numOfRooms;
-		this.numOfBathrooms = numOfBathrooms;
-		this.isForSale = isForSale;
-		this.estateValue = estateValue;
-		this.userDTO = userDTO;
-	}
-
-	public EstateDTO() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEstateCode() {
-		return estateCode;
-	}
-
-	public void setEstateCode(String estateCode) {
-		this.estateCode = estateCode;
-	}
-
-	public float getArea() {
-		return area;
-	}
-
-	public void setArea(float area) {
-		this.area = area;
-	}
-
-	public int getNumOfRooms() {
-		return numOfRooms;
-	}
-
-	public void setNumOfRooms(int numOfRooms) {
-		this.numOfRooms = numOfRooms;
-	}
-
-	public int getNumOfBathrooms() {
-		return numOfBathrooms;
-	}
-
-	public void setNumOfBathrooms(int numOfBathrooms) {
-		this.numOfBathrooms = numOfBathrooms;
-	}
-
-	public boolean isForSale() {
-		return isForSale;
-	}
-
-	public void setForSale(boolean isForSale) {
-		this.isForSale = isForSale;
-	}
-
-	public float getEstateValue() {
-		return estateValue;
-	}
-
-	public void setEstateValue(float estateValue) {
-		this.estateValue = estateValue;
-	}
-
-	public UserDTO getUserDTO() {
-		return userDTO;
-	}
-
-	public void setUserDTO(UserDTO userDTO) {
-		this.userDTO = userDTO;
-	}
-	
+			
 }
--- src/main/java/co/com/udem/rea/entity/Estate.java
+++ src/main/java/co/com/udem/rea/entity/Estate.java
@@ -4,16 +4,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Estate {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String estateCode;
+	private int estateCode;
 	private float area;
 	private int numOfRooms;
 	private int numOfBathrooms;
@@ -21,86 +29,7 @@ public class Estate {
 	private float estateValue;
 	
 	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
 	private User user;
 	
-	public Estate(Long id, String estateCode, float area, int numOfRooms, int numOfBathrooms, boolean isForSale,
-			float estateValue, User user) {
-		super();
-		this.id = id;
-		this.estateCode = estateCode;
-		this.area = area;
-		this.numOfRooms = numOfRooms;
-		this.numOfBathrooms = numOfBathrooms;
-		this.isForSale = isForSale;
-		this.estateValue = estateValue;
-		this.user = user;
-	}
-
-	public Estate() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEstateCode() {
-		return estateCode;
-	}
-
-	public void setEstateCode(String estateCode) {
-		this.estateCode = estateCode;
-	}
-
-	public float getArea() {
-		return area;
-	}
-
-	public void setArea(float area) {
-		this.area = area;
-	}
-
-	public int getNumOfRooms() {
-		return numOfRooms;
-	}
-
-	public void setNumOfRooms(int numOfRooms) {
-		this.numOfRooms = numOfRooms;
-	}
-
-	public int getNumOfBathrooms() {
-		return numOfBathrooms;
-	}
-
-	public void setNumOfBathrooms(int numOfBathrooms) {
-		this.numOfBathrooms = numOfBathrooms;
-	}
-
-	public boolean isForSale() {
-		return isForSale;
-	}
-
-	public void setForSale(boolean isForSale) {
-		this.isForSale = isForSale;
-	}
-
-	public float getEstateValue() {
-		return estateValue;
-	}
-
-	public void setEstateValue(float estateValue) {
-		this.estateValue = estateValue;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
 }
